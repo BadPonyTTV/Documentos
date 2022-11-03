435bbcbc512302e13434a9d9a505f64fd5ec5dd3
BadPony_TTv <asap.badpony@gmail.com>
2022-11-02 23:49:23 -0600

    commit inicial

---
+++ My Games/Age of Empires IV/Users/2535450007675053/configuration_user.lua
@@ -0,0 +1,492 @@
+configuration =  
+{
+	[1] =  
+	{
+		setting = "tooltipdelay",
+		variantFloat = 0.00000,
+	},
+	[2] =  
+	{
+		setting = "subtitlebackgroundcolor",
+		variantUInt = 0,
+	},
+	[3] =  
+	{
+		setting = "buildprogressalwayson",
+		variantBool = true,
+	},
+	[5] =  
+	{
+		setting = "scrollwheelzoom",
+		variantBool = true,
+	},
+	[6] =  
+	{
+		setting = "commandqueuemode",
+		variantInt = 0,
+	},
+	[12] =  
+	{
+		setting = "pingchatmessages",
+		variantBool = true,
+	},
+	[13] =  
+	{
+		setting = "eventmajorgamemodeobjectives",
+		variantBool = true,
+	},
+	[14] =  
+	{
+		setting = "constructionprogressvisibilitymode",
+		variantInt = 2,
+	},
+	[15] =  
+	{
+		setting = "advancedorders",
+		variantBool = false,
+	},
+	[16] =  
+	{
+		setting = "autoselectserverregion",
+		variantBool = true,
+	},
+	[18] =  
+	{
+		setting = "campaigndifficulty",
+		variantInt = 2,
+	},
+	[19] =  
+	{
+		setting = "playercolour",
+		variantBool = false,
+	},
+	[20] =  
+	{
+		setting = "minimapresources",
+		variantBool = true,
+	},
+	[21] =  
+	{
+		setting = "buttonholdtiming",
+		variantFloat = 3.00000,
+	},
+	[22] =  
+	{
+		setting = "showcontrolgroup",
+		variantBool = true,
+	},
+	[26] =  
+	{
+		setting = "districttiericonvisibilitymode",
+		variantInt = 2,
+	},
+	[27] =  
+	{
+		setting = "swapshiftandaltkeys",
+		variantBool = false,
+	},
+	[28] =  
+	{
+		setting = "uniqueminimapplayericons",
+		variantBool = false,
+	},
+	[29] =  
+	{
+		setting = "camerarotation",
+		variantBool = true,
+	},
+	[31] =  
+	{
+		setting = "eventunitcomplete",
+		variantBool = true,
+	},
+	[33] =  
+	{
+		setting = "eventbuildingcomplete",
+		variantBool = true,
+	},
+	[34] =  
+	{
+		setting = "cyclesubselectionpicktype",
+		variantInt = 1,
+	},
+	[36] =  
+	{
+		setting = "rightclickgarrison",
+		variantBool = true,
+	},
+	[37] =  
+	{
+		setting = "eventageup",
+		variantBool = true,
+	},
+	[38] =  
+	{
+		setting = "showdamageflashing",
+		variantBool = false,
+	},
+	[39] =  
+	{
+		setting = "showcanissueresult",
+		variantBool = true,
+	},
+	[41] =  
+	{
+		setting = "showplayerscores",
+		variantBool = true,
+	},
+	[42] =  
+	{
+		setting = "eventminorcampaignobjectives",
+		variantBool = true,
+	},
+	[43] =  
+	{
+		setting = "buildinggridoverlay",
+		variantInt = 1,
+	},
+	[44] =  
+	{
+		setting = "displaynoaudioicon",
+		variantBool = false,
+	},
+	[45] =  
+	{
+		setting = "activescreenedgepanhandler",
+		variantString = "screen_edge_pan_default",
+	},
+	[46] =  
+	{
+		setting = "eventupgradecomplete",
+		variantBool = true,
+	},
+	[48] =  
+	{
+		setting = "minimappingnotifications",
+		variantBool = true,
+	},
+	[49] =  
+	{
+		setting = "edgepanspeedfactor",
+		variantFloat = 1.00000,
+	},
+	[50] =  
+	{
+		setting = "onlinepresence",
+		variantBool = true,
+	},
+	[51] =  
+	{
+		setting = "idlevillagerpicktype",
+		variantInt = 1,
+	},
+	[52] =  
+	{
+		setting = "uielementnarration",
+		variantBool = false,
+	},
+	[53] =  
+	{
+		setting = "stickyselection",
+		variantBool = false,
+	},
+	[54] =  
+	{
+		setting = "allowfriendrequests",
+		variantUInt = 0,
+	},
+	[58] =  
+	{
+		setting = "edgepanacceleration",
+		variantFloat = 1.00000,
+	},
+	[59] =  
+	{
+		setting = "showformationaction",
+		variantBool = true,
+	},
+	[61] =  
+	{
+		setting = "enabletaunts",
+		variantBool = true,
+	},
+	[62] =  
+	{
+		setting = "minimapunits",
+		variantBool = true,
+	},
+	[64] =  
+	{
+		setting = "singleactionwaypointmarkers",
+		variantBool = false,
+	},
+	[65] =  
+	{
+		setting = "gamewindowactivewhenloaded",
+		variantBool = false,
+	},
+	[66] =  
+	{
+		setting = "showleadercrown",
+		variantBool = true,
+	},
+	[67] =  
+	{
+		setting = "showgametimer",
+		variantBool = false,
+	},
+	[68] =  
+	{
+		setting = "mousebuttonpandirection",
+		variantString = "reversed",
+	},
+	[69] =  
+	{
+		setting = "eventunderattack",
+		variantBool = true,
+	},
+	[70] =  
+	{
+		setting = "showidlevillagericons",
+		variantBool = true,
+	},
+	[71] =  
+	{
+		setting = "reduceflashes",
+		variantBool = false,
+	},
+	[72] =  
+	{
+		setting = "minimapbuildnotifications",
+		variantBool = true,
+	},
+	[77] =  
+	{
+		setting = "minimapobjects",
+		variantBool = true,
+	},
+	[79] =  
+	{
+		setting = "eventunitfound",
+		variantBool = true,
+	},
+	[81] =  
+	{
+		setting = "readincomingtextchat",
+		variantBool = false,
+	},
+	[82] =  
+	{
+		setting = "texttospeechforchatmessages",
+	},
+	[83] =  
+	{
+		setting = "landmarkconstructionprogressvisibilitymode",
+		variantBool = true,
+	},
+	[84] =  
+	{
+		setting = "slidingnotifications",
+		variantBool = true,
+	},
+	[85] =  
+	{
+		setting = "eventobjectfound",
+		variantBool = true,
+	},
+	[86] =  
+	{
+		setting = "usevoicechat",
+		variantBool = true,
+	},
+	[89] =  
+	{
+		setting = "showhudsubtitles",
+		variantBool = true,
+	},
+	[90] =  
+	{
+		setting = "classicxpkickers",
+		variantBool = true,
+	},
+	[91] =  
+	{
+		setting = "customizegamecolors",
+		variantUInt = 0,
+	},
+	[93] =  
+	{
+		setting = "allowmessages",
+		variantUInt = 0,
+	},
+	[94] =  
+	{
+		setting = "edgepan",
+		variantBool = true,
+	},
+	[97] =  
+	{
+		setting = "focusselectedfollow",
+		variantInt = 1,
+	},
+	[98] =  
+	{
+		setting = "speechtotext",
+		variantBool = false,
+	},
+	[99] =  
+	{
+		setting = "idlemilitarypicktype",
+		variantInt = 1,
+	},
+	[101] =  
+	{
+		setting = "displaykeyboardchaticon",
+		variantBool = false,
+	},
+	[102] =  
+	{
+		setting = "showunitdescription",
+		variantBool = true,
+	},
+	[104] =  
+	{
+		setting = "bandboxselect",
+		variantKey = "",
+	},
+	[106] =  
+	{
+		setting = "eventpopulationcap",
+		variantBool = true,
+	},
+	[107] =  
+	{
+		setting = "tutorialinterfaceenabled",
+		variantBool = true,
+	},
+	[108] =  
+	{
+		setting = "dynamictraining",
+		variantBool = true,
+	},
+	[109] =  
+	{
+		setting = "voicechatmethod",
+		variantUInt = 0,
+	},
+	[110] =  
+	{
+		setting = "keyboardpanacceleration",
+		variantFloat = 1.00000,
+	},
+	[113] =  
+	{
+		setting = "showcustomitems",
+		variantBool = true,
+	},
+	[115] =  
+	{
+		setting = "findandcyclepicktype",
+		variantInt = 1,
+	},
+	[116] =  
+	{
+		setting = "campaignautosave",
+		variantBool = true,
+	},
+	[119] =  
+	{
+		setting = "enablepanaccelerationandsmoothing",
+		variantBool = true,
+	},
+	[122] =  
+	{
+		setting = "layoutcommandcardright",
+		variantBool = true,
+	},
+	[125] =  
+	{
+		setting = "globalbuildqueuevisibility",
+		variantInt = 0,
+	},
+	[128] =  
+	{
+		setting = "keyboardpanspeedfactor",
+		variantFloat = 1.00000,
+	},
+	[131] =  
+	{
+		setting = "showpaths",
+		variantBool = true,
+	},
+	[134] =  
+	{
+		setting = "healthbarvisibilitymode",
+		variantInt = 0,
+	},
+	[137] =  
+	{
+		setting = "shownoncriticaleventcues",
+		variantBool = true,
+	},
+	[138] =  
+	{
+		setting = "campaigndifficultyset",
+		variantBool = true,
+	},
+	[143] =  
+	{
+		setting = "exclusivecontrolgroups",
+		variantBool = false,
+	},
+	[146] =  
+	{
+		setting = "cameramode",
+		variantInt = 0,
+	},
+	[147] =  
+	{
+		setting = "showunitocclusion",
+		variantBool = true,
+	},
+	[151] =  
+	{
+		setting = "minimapattacknotifications",
+		variantBool = true,
+	},
+	[153] =  
+	{
+		setting = "showchattimestamp",
+		variantBool = false,
+	},
+	[154] =  
+	{
+		setting = "blockugc",
+		variantBool = false,
+	},
+	[155] =  
+	{
+		setting = "activemousepanhandler",
+		variantString = "mouse_pan_default",
+	},
+	[156] =  
+	{
+		setting = "minimapbuildings",
+		variantBool = true,
+	},
+	[157] =  
+	{
+		setting = "activekeyboardpanhandler",
+		variantString = "keyboard_pan_default",
+	},
+	[160] =  
+	{
+		setting = "eventminorgamemodeobjectives",
+		variantBool = true,
+	},
+}
+migrations =  
+{
+}
+version = 3666938
+
